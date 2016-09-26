347d7c2f9a3d61cd691ca0da64ec622161c5a9c2
alienwizard <johanwendin@gmail.com>
2016-09-26 12:32:20 +0200

    pusing for staging

--- wp-config.staging.php
+++ wp-config.staging.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'blomsterkringlan');
+define('DB_NAME', 'Blomsterkringlan');
 
 /** MySQL database username */
 define('DB_USER', '426e095597a3');
