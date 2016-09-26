ce9fc12bcdfef3558e5fd94f0afbe232b008b7b5
alienwizard <johanwendin@gmail.com>
2016-09-26 15:21:15 +0200

    finished wp programming

--- wp-config.staging.php
+++ wp-config.staging.php
@@ -37,6 +37,10 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+
+define('WP_HOME', 'http://blomsterkringlan.mediahelpcrm.se/');
+define('WP_SITEURL', 'http://blomsterkringlan.mediahelpcrm.se/');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
